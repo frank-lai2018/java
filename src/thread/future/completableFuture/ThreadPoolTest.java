6026e849f3efada86c36aa6c5394eb2ec717da74
FRANK LAI <jianhanlai@gmail.com>
2022-04-12 23:13:14 +0800

    20220412

--- src/thread/future/completableFuture/ThreadPoolTest.java
+++ src/thread/future/completableFuture/ThreadPoolTest.java
@@ -3,6 +3,9 @@ package thread.future.completableFuture;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 /**
@@ -11,7 +14,8 @@ import java.util.stream.IntStream;
 public class ThreadPoolTest {
 
     public static void main(String[] args) {
-        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+//        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        ExecutorService threadPool = getThreadPoolExecutor();
 
         long startTime = System.currentTimeMillis();
 
@@ -34,4 +38,10 @@ public class ThreadPoolTest {
     	SmallTool.sleepMillis(3000);
     	SmallTool.printTimeAndThread(i+"執行緒執行完了");
     }
+    
+    public static ThreadPoolExecutor getThreadPoolExecutor() {
+    	return new ThreadPoolExecutor(2, 5,  20L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(2));
+    }
+    
+    
 }
